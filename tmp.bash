8c1b86225f01032d9f56ff44894285ea54820d2c
Jeremy Wall <Jeremy@marzhillstudios.com>
2024-11-30 09:19:30 -0500

    chore: extra bash file crept in

--- tmp.bash
+++
@@ -1,9 +0,0 @@
-set -ue
-set -o pipefail
-export BUILD_ARG_one="widget"
-export BUILD_ARG_two="acme"
-VALS=()
-printenv | grep BUILD_ARG | while read line;do
-  VALS+=($line)
-done
-echo "${VALS[*]}"
